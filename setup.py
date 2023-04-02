f5a03e9240155139febf4a2643acf2c254e81595
AlexTraveylan <monsieurdemares@gmail.com>
2023-04-02 21:25:57 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="solgard-bot",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "solgard-bot = solgard-bot-last-refactoring.app:main",
+        ]
+    },
+    author="Alex Traveylan",
+    author_email="alex.traveylan@gmail.com",
+    description="A bot for legend of solgard",
+    url="https://github.com/AlexTraveylan/solgard-bot-last-refactoring",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
