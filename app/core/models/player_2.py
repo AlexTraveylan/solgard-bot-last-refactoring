e1bf4cfdcc94f3236da67fc65959f6058ef8de2c
AlexTraveylan <monsieurdemares@gmail.com>
2023-05-26 23:15:19 +0200

    ab fonctionnel, mais bux sur les jours precedents

--- app/core/models/player_2.py
+++ app/core/models/player_2.py
@@ -1,7 +1,50 @@
 from dataclasses import dataclass, field
+import datetime
+from typing import Literal
 
 from app.core.entrypoint.json_api import JsonAPI
 from app.core.models.api_solgard import ApiSolgard
+
+
+def debutJourneeByTimecode(timecode: int) -> int:
+    dt = datetime.datetime.fromtimestamp(timecode / 1000.0)
+    day_start = dt.replace(hour=7, minute=0, second=0, microsecond=0)
+
+    if dt.hour < 7:
+        day_start = day_start - datetime.timedelta(days=1)
+
+    begin_day_timestamp = int(day_start.timestamp() * 1000)
+
+    return begin_day_timestamp
+
+
+def is_on_day(nb_day_passed: Literal[0, 1, 2, 3, 4, 5], begin_day: int, actual_timestamp: int) -> bool:
+    one_day_timestamp = 24 * 60 * 60 * 1000
+    if nb_day_passed == 0:
+        return actual_timestamp > begin_day
+    else:
+        return begin_day - one_day_timestamp * nb_day_passed < actual_timestamp < begin_day - one_day_timestamp * (nb_day_passed - 1)
+
+
+DEFAULT_COUNT_DICT = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+
+
+@dataclass
+class MemberBombAttacks:
+    member_id: str
+    nb_bomb_used_by_day: dict[int, int] = field(default_factory=lambda: DEFAULT_COUNT_DICT.copy())
+    nb_attacks_used_by_day: dict[int, int] = field(default_factory=lambda: DEFAULT_COUNT_DICT.copy())
+
+
+@dataclass
+class BombsAttacks:
+    members_bomb_attacks: list[MemberBombAttacks] = field(default_factory=list)
+
+    def find_member_by_member_id(self, user_id: str):
+        for member in self.members_bomb_attacks:
+            if member.member_id == user_id:
+                return member
+        raise ValueError("Member not found")
 
 
 @dataclass
@@ -13,6 +56,7 @@ class Player_2_data:
     guild_id: str = None
     guild_name: str = None
     guild_members: dict[str, str] = field(default_factory=dict)
+    bombs_attacks: BombsAttacks = BombsAttacks()  # element 0 will be total
     # dependancies
     json_api = JsonAPI()
 
@@ -20,6 +64,7 @@ class Player_2_data:
         self.api_solgard = ApiSolgard(self.user_id, self.session_id)
         self._set_play_2_content()
         self._set_guild_infos()
+        self._set_bombs_info()
 
     def _set_play_2_content(self) -> None:
         """get the player_2_content from solgard_api and set it"""
@@ -40,3 +85,55 @@ class Player_2_data:
             member_user_id = member["userId"]
             member_name = member["displayName"]
             self.guild_members[member_user_id] = member_name
+            self.bombs_attacks.members_bomb_attacks.append(MemberBombAttacks(member_user_id))
+
+    def _set_bombs_info(self):
+        """set bombs and attacks for eatch members 5 days in the past"""
+
+        player = self.play_2_content["eventResult"]["eventResponseData"]["player"]
+        actual_timecode: int = player["timestamp"]
+        begin_day = debutJourneeByTimecode(actual_timecode)
+
+        guildActivities: list[dict[str, any]] = player["guild"]["guildActivities"]
+
+        for activity in guildActivities:
+            member_concerned_id: str = activity["userId"]
+            member_concerned: MemberBombAttacks = self.bombs_attacks.find_member_by_member_id(member_concerned_id)
+            activity_timestamp: int = activity["createdOn"]
+            isBomb: bool = activity["type"] == "GUILD_BOSS_BOMB"
+
+            if is_on_day(0, begin_day, activity_timestamp):
+                if isBomb:
+                    member_concerned.nb_bomb_used_by_day[0] += 1
+                else:
+                    member_concerned.nb_attacks_used_by_day[0] += 1
+
+            if is_on_day(1, begin_day, activity_timestamp):
+                if isBomb:
+                    member_concerned.nb_bomb_used_by_day[1] += 1
+                else:
+                    member_concerned.nb_attacks_used_by_day[1] += 1
+
+            if is_on_day(2, begin_day, activity_timestamp):
+                if isBomb:
+                    member_concerned.nb_bomb_used_by_day[2] += 1
+                else:
+                    member_concerned.nb_attacks_used_by_day[2] += 1
+
+            if is_on_day(3, begin_day, activity_timestamp):
+                if isBomb:
+                    member_concerned.nb_bomb_used_by_day[3] += 1
+                else:
+                    member_concerned.nb_attacks_used_by_day[3] += 1
+
+            if is_on_day(4, begin_day, activity_timestamp):
+                if isBomb:
+                    member_concerned.nb_bomb_used_by_day[4] += 1
+                else:
+                    member_concerned.nb_attacks_used_by_day[4] += 1
+
+            if is_on_day(5, begin_day, activity_timestamp):
+                if isBomb:
+                    member_concerned.nb_bomb_used_by_day[5] += 1
+                else:
+                    member_concerned.nb_attacks_used_by_day[5] += 1
--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import io
+from typing import Literal
 from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 import os
+import datetime
 
 from app.core.models.connect_user import ConnectUser
 from app.core.models.player_2 import Player_2_data
@@ -23,7 +25,20 @@ async def on_ready():
 
 @bot.command("test")
 async def test(context: commands.Context):
-    await context.send("Test reussie avec succes")
+    now = datetime.datetime.now()
+    colour = discord.Colour.blue()
+    embed = discord.Embed(
+        title="Test embed de test",
+        color=discord.Color.yellow(),
+        description="Alors, blabla blablabla blabalblabalblabla. Voila\nThe end.",
+        timestamp=now,
+        type="image",
+        colour=colour,
+        url="https://img.freepik.com/photos-premium/image-galaxie-coloree-dans-ciel-ai-generative_791316-9864.jpg?w=2000",
+    )
+    embed.set_image(url="https://img.freepik.com/photos-premium/image-galaxie-coloree-dans-ciel-ai-generative_791316-9864.jpg?w=2000")
+    embed.add_field(name="2eme champ", value="contenu du 2eme champ")
+    await context.send(embed=embed)
 
 
 @bot.command("connect_test")
@@ -52,6 +67,56 @@ async def get_play_2(context: commands.Context):
     response = message.getvalue()
 
     await context.send(response)
+
+
+@bot.command("ab")
+async def ab(context: commands.Context, nb_day: Literal[0, 1, 2, 3, 4, 5] = 0):
+    user = ConnectUser()
+    user.connect_and_get_new_session_id()
+    play_2 = Player_2_data(*user.get_user_id_session_id())
+
+    title = "Attaque(s) et bombe(s)"
+    if nb_day == 0:
+        description = "Attaques et bombes restantes de la journée"
+    else:
+        description = (
+            f"Attaque(s) et bombe(s) manquante(s) {nb_day} jour(s) en arriere.\nJ'ai un bug non géré si vous demandez le mercredi de pause.\n"
+        )
+
+    now = datetime.datetime.now()
+    colour = discord.Colour.blue()
+
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        timestamp=now,
+        colour=colour,
+    )
+
+    members = play_2.bombs_attacks.members_bomb_attacks
+    members_missing_something = [
+        member for member in members if (member.nb_attacks_used_by_day[nb_day] == 0 or member.nb_bomb_used_by_day[nb_day] == 0)
+    ]
+
+    for member in members_missing_something:
+        member_name = play_2.guild_members[member.member_id]
+
+        member_nb_atck = member.nb_attacks_used_by_day[nb_day]
+        is_attack_done = member_nb_atck == 2
+        if is_attack_done:
+            display_atck = "Toutes les attaques effectués"
+        else:
+            display_atck = f"{2 - member_nb_atck} attaques restantes"
+
+        member_nb_bomb = member.nb_bomb_used_by_day[nb_day]
+        is_bomb_done = member_nb_bomb == 1
+        if is_bomb_done:
+            display_bomb = ""
+        else:
+            display_bomb = "Bombe non utilisée"
+        embed.add_field(name=f"{member_name}", value=f"{display_atck}\n{display_bomb}", inline=False)
+
+    await context.send(embed=embed)
 
 
 bot.run(BOT_KEY)
